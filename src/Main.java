95c0857befade1f99653457d7d983510285c4761
Alex Kleschenko <alexkv2694@gmail.com>
2022-01-05 13:37:31 +0300

    Code reformat

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int topUp =1100;
-        int bonus = topUp/100;
-        if (topUp <=1000) {
-            bonus = 0;
+        int topUp = 1100;
+        int bonus = topUp / 100;
+        {
+            if (topUp <= 1000) {
+                bonus = 0;
+            }
         }
-        int totalBalance = balance+topUp+bonus;
+        int totalBalance = balance + topUp + bonus;
 
         System.out.println("Ваш бонус:" + bonus);
         System.out.println("Ваш баланс:" + totalBalance);
